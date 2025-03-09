6dbf5a27f91adc7820c10ee8d5a2ace059c1f5cd
Muhammad Faiq <muhammadfaiqwaseem3@gmail.com>
2025-03-09 11:52:27 +0500

    chapter 1 to 05

---
+++ app.js
@@ -0,0 +1,70 @@
+// ---------chapter 01 question ----
+
+//Q.01
+
+// alert("")
+
+// Q.02
+
+// alert("Error! Please enter a valid pasworrd")
+
+// Q.03
+
+// var linbreak ="Welcom to js land\n Happy codding"
+
+// alert(linbreak)
+
+// Q.04
+
+// var practice1n = "welcom to js land"
+
+// alert(practice1n)
+
+// var practice1n = "Happy codding"
+
+// alert(practice1n)
+
+// Q.05
+
+// var print = "Hello.. Ican Js through my web browser's console"
+
+// console.log(print)
+
+// ---------chapter 02 questi
+
+//Q.01
+
+//  var chap = "Faiq"
+//  alert(chap)
+//  var chap = "15 years"
+//  alert(chap)
+//  var chap = "certified mobile application development"
+//  alert(chap)
+
+// Q.02
+
+// var chap = "PIZZA\n PIZZ\n PIZ\n PI\n P"
+// alert(chap)
+
+// Q.03
+
+// var Email = "my email address is muhammadfaiqwaseem@gmail.com"
+
+// alert(Email)
+
+// Q.04
+
+//  var book = "JavaScript Exercises for A Smarterway To Learn JavaScript Book"
+
+//  alert(book)
+
+// Q.05
+// yah i cna write html content through javasript
+
+// Q.06
+
+// var smbal = "----------------------&&**&&-----------------------"
+
+// alert(smbal)
+
+// ---------chapter 03 questi
